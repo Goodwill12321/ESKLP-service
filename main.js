9d6036dc36540e682d1b32aca23494ef46a2e6e0
VK <kostenich.vv@gmail.com>
2025-07-21 22:43:51 +0400

    adding version to message on main page

--- main.js
+++ main.js
@@ -874,7 +874,7 @@ app.get('/', async function(req, res) {
         // Получаем дату последнего обновления через вашу функцию
         const updateInfo = await getLastUpdateInfo(db);
 
-        let html = '<!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><title>Информация о базе ЕСКЛП</title></head><body>';
+        let html = '<!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><title>Информация о базе ЕСКЛП v1</title></head><body>';
         html += '<h1>Информация о базе ЕСКЛП</h1><div>';
 
         if (updating) {
